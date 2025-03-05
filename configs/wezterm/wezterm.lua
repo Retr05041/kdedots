83d4823936e3ee330e8551c34becaf4e9f1f6042
Retr05041 <parker@cranfield.ca>
2025-03-05 19:52:50 +0000

    Made wezterm work with new font

--- configs/wezterm/wezterm.lua
+++ configs/wezterm/wezterm.lua
@@ -6,6 +6,5 @@ config.color_scheme = "Tokyo Night"
 config.enable_tab_bar = false
 config.enable_scroll_bar = false
 config.window_background_opacity = 0.9
-config.font = wezterm.font("Hack Nerd Font")
 
 return config
